f39cf7b1060698a5f5d7a5bc92570d7e26996b06
Roberto Penna <robpenn95@gmail.com>
2017-04-19 16:39:40 +0200

    populateDB funziona
    Modificato il programmino, ora funziona. Bisogna scrivere una query "giusta", attenzione ai separatori.

--- populateDB.py
+++ populateDB.py
@@ -1,8 +1,26 @@
 import pymysql
-import csv
 
-mydb = pymysql.connect(host='localhost',
-                       user='co302mc',
-                       passwd='mafol597',
-                       db='co302mc_bike_theft')
-cursor = mydb.cursor()
+# apro una connessione al DB
+db = pymysql.connect(host='localhost',
+                     user='root',
+                     password='carlitos')
+
+# assegno un cursore
+cursor = db.cursor()
+
+# operazioni del cursore
+cursor.execute("USE mind;")
+
+sql = """"""
+
+try:
+    # Eseguo il comando
+    cursor.execute(sql)
+    # Committo i cambiamenti
+    db.commit()
+except:
+    # In caso di errori
+    db.rollback()
+
+# chiudo la connessione
+db.close()
